e97eda15ffd1864d3ea17322cfa617bdd1f55ea2
Zoka72VL <105391352+Zoka72VL@users.noreply.github.com>
2025-02-04 20:08:49 -0300

    Html Usuario
    se creo el componente barra y la logica!

--- Casa de apuestas/frontend/js/cargarHeader.js
+++ Casa de apuestas/frontend/js/cargarHeader.js
@@ -8,3 +8,16 @@ function cargarHeader() {
     .catch((error) => console.error("Error al cargar el header:", error)); // Manejo de errores
 }
 cargarHeader();
+
+// Función que carga la barra dinamicamente
+function cargarBarra() {
+  fetch("./components/barra.html") // La ruta
+    .then((response) => response.text()) // Obtener el contenido como texto
+    .then((data) => {
+      document.getElementById("barra-container").innerHTML = data; // Inserta el header en el contenedor
+    })
+    .catch((error) => console.error("Error al cargar el header:", error)); // Manejo de errores
+}
+cargarBarra();
+
+
