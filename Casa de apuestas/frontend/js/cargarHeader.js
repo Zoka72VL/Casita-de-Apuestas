00ec8367ebf5136cc06ea9a757a2b39751ae719a
RioSebastianR <riossebastianr@gmail.com>
2025-02-04 17:14:11 -0300

    creacion del componente header y su logica

---
+++ Casa de apuestas/frontend/js/cargarHeader.js
@@ -0,0 +1,10 @@
+// Función que carga el header dinámicamente
+function cargarHeader() {
+  fetch("./components/component-header.html") // La ruta
+    .then((response) => response.text()) // Obtener el contenido como texto
+    .then((data) => {
+      document.getElementById("header-container").innerHTML = data; // Inserta el header en el contenedor
+    })
+    .catch((error) => console.error("Error al cargar el header:", error)); // Manejo de errores
+}
+cargarHeader();
