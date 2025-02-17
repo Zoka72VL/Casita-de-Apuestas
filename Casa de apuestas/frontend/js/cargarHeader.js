b0470b8177e5ff646324f3dde8d3fb909df1708d
Zoka72VL <105391352+Zoka72VL@users.noreply.github.com>
2025-02-17 20:29:10 -0300

    Vista Admin Caster
    Se usaron scrip para que aparezcan los formularios correspondientes, hay que dividirlos y revisarlos c/u para q tengan coherencia

--- Casa de apuestas/frontend/js/cargarHeader.js
+++ Casa de apuestas/frontend/js/cargarHeader.js
@@ -20,14 +20,35 @@ function cargarBarra() {
 }
 cargarBarra();
 
-function cargarEvento() {
-  fetch("./components/eventos.html") // La ruta
+function formularioEvento() {                           
+  fetch("./components/formularioEvento.html") // La ruta
     .then((response) => response.text()) // Obtener el contenido como texto
     .then((data) => {
-      document.getElementById("eventos-container").innerHTML = data; // Inserta el header en el contenedor
+      document.getElementById("FormEventos-container").innerHTML = data; // Inserta el header en el contenedor
     })
-    .catch((error) => console.error("Error al cargar el evento:", error)); // Manejo de errores
+    .catch((error) => console.error("Error al cargar el formulario para crear evento:", error)); // Manejo de errores
 }
-cargarEvento();
+
+function crearJugador() {
+  fetch("./components/formularioJugador.html") // La ruta
+    .then((response) => response.text()) // Obtener el contenido como texto
+    .then((data) => {
+      document.getElementById("FormJugador-container").innerHTML = data; // Inserta el header en el contenedor
+    })
+    .catch((error) => console.error("Error al cargar el formulario para crear jugador:", error)); // Manejo de errores
+}
+
+
+
+function formularioUsuario() {
+  fetch("./components/formularioUsuario.html") // La ruta
+    .then((response) => response.text()) // Obtener el contenido como texto
+    .then((data) => {
+      document.getElementById("FormUsuario-container").innerHTML = data; // 
+    })
+    .catch((error) => console.error("Error al cargar el formulario para modificar Usuario:", error)); // Manejo de errores
+}
+
+
 
 
