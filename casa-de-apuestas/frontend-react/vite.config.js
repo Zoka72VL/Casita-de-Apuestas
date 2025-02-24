9fe56b472b0498b9b1eabf943927f44c9d8e39b2
RioSebastianR <riossebastianr@gmail.com>
2025-02-24 20:54:01 -0300

    se agregaron las dependencias automaticamente en package-lock-json

--- casa-de-apuestas/frontend-react/vite.config.js
+++ casa-de-apuestas/frontend-react/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Usamos 'import.meta.url' y 'fileURLToPath' para obtener el directorio actual
+      '~bootstrap': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'node_modules/bootstrap'),
+    }
+  },
 })
