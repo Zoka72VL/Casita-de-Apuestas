63eebd640f53389375549b6c4a02fc23a880e431
RioSebastianR <riossebastianr@gmail.com>
2025-02-25 18:39:36 -0300

    cambio los nombres , pero mas ser mas ordenado simplemente,saber que se refieren a pages(rutas) y no a componentes

--- casa-de-apuestas/frontend-react/src/main.jsx
+++ casa-de-apuestas/frontend-react/src/main.jsx
@@ -1,23 +1,24 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
+// import HomePage from "./pages/HomePage.jsx";
 // eslint-disable-next-line no-unused-vars
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 import "./css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "./css/tooplate-kool-form-pack.css";
 
-import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter/>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
           
         
       </Routes>
-    <BrowserRouter/>
+    </BrowserRouter>
   </StrictMode>
 );
