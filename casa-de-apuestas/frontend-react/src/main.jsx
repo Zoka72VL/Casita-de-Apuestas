72954e3776594e22f1d480b893be5803aa492c61
RioSebastianR <riossebastianr@gmail.com>
2025-02-25 18:08:34 -0300

    comenzando el roteo

--- casa-de-apuestas/frontend-react/src/main.jsx
+++ casa-de-apuestas/frontend-react/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 // eslint-disable-next-line no-unused-vars
 import * as bootstrap from 'bootstrap'
@@ -8,9 +8,16 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 import "./css/tooplate-kool-form-pack.css";
 
 import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter/>
+      <Routes>
+        <Route path="/" element={<App />} />
+          
+        
+      </Routes>
+    <BrowserRouter/>
   </StrictMode>
 );
