7d2b04974415b4507007e82c8220442eec13443d
RioSebastianR <riossebastianr@gmail.com>
2025-02-24 21:00:44 -0300

    actualizamos main , sera el encargado de llevar acabo el ruteo de las pages , en este archivo se van a definir

--- casa-de-apuestas/frontend-react/src/main.jsx
+++ casa-de-apuestas/frontend-react/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'
+import "./css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./css/tooplate-kool-form-pack.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
