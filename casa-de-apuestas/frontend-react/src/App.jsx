eddc72a87bdcfa2ce24f72906ddc0bd88eb38018
RioSebastianR <riossebastianr@gmail.com>
2025-02-26 16:56:38 -0300

    a App.jsx se le agrego q importe Outlet es para anidar rutas

--- casa-de-apuestas/frontend-react/src/App.jsx
+++ casa-de-apuestas/frontend-react/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/layout/Header.jsx";
 import Content from "./components/layout/Content.jsx";
 import Video from "./components/UI/Video.jsx";
+import { Outlet } from "react-router";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Content />
       <Video />
+      <Outlet/>
     </>
   );
 }
