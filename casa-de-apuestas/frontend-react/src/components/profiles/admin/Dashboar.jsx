b2faf2c3131e3c179f6d655cc02d5f108bde1bea
Zoka72VL <105391352+Zoka72VL@users.noreply.github.com>
2025-05-23 17:09:09 -0300

    Update Dashboar.jsx
    cambio de buttom a link

--- casa-de-apuestas/frontend-react/src/components/profiles/admin/Dashboar.jsx
+++ casa-de-apuestas/frontend-react/src/components/profiles/admin/Dashboar.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const Dashboar = () => {
   return (
@@ -8,26 +6,32 @@ const Dashboar = () => {
       <div className="container container-custom">
         <div className="row frente">
           <div className="col-lg-6 col-md-8 col-12 mx-auto">
-          
-<form className="custom-form login-form" role="form" method="post">
-  <div className="col-lg-12 col-12 mb-3">
-    <div className="form-floating p-0">
-      <button type="submit" className="form-control">Panel Usuario</button>
-    </div>
-  </div>
-
-  <div className="col-lg-12 col-12 mb-3">
-    <div className="form-floating p-0">
-      <button type="submit" className="form-control">Panel Administracion</button>
-    </div>
-  </div>
-
-  <div className="col-lg-12 col-12">
-    <div className="form-floating p-0">
-      <button type="submit" className="form-control">Ingresar a La Casita</button>
-    </div>
-  </div>
-</form>
+            <div className="row justify-content-center align-items-center">
+              <div className="col-12 col-md-7 text-center mb-3">
+                <NavLink
+                  className="btn custom-btn custom-border-btn w-100"
+                  to="/usuario"
+                >
+                  Panel Usuario
+                </NavLink>
+              </div>
+              <div className="col-12 col-md-7 text-center mb-3">
+                <NavLink
+                  className="btn custom-btn custom-border-btn w-100"
+                  to="/admin"
+                >
+                  Panel Administracion
+                </NavLink>
+              </div>
+              <div className="col-12 col-md-7 text-center">
+                <NavLink
+                  className="btn custom-btn custom-border-btn w-100"
+                  to="/casita"
+                >
+                  Ingresar a La Casita
+                </NavLink>
+              </div>
+            </div>
           </div>
         </div>
       </div>
