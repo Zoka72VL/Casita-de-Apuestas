3fdfb7912080e14e110278337258de41f25bd958
RioSebastianR <riossebastianr@gmail.com>
2025-05-25 20:12:49 -0300

    se corrigio la clase del button "enviar respuesta" (ahora quedo centrado)

--- casa-de-apuestas/frontend-react/src/components/profiles/admin/ResponderTrivia.jsx
+++ casa-de-apuestas/frontend-react/src/components/profiles/admin/ResponderTrivia.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { NavLink } from "react-router";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { NavLink } from "react-router";
 
 const ResponderTrivia = () => {
   return (
@@ -8,29 +8,34 @@ const ResponderTrivia = () => {
       <div className="container container-custom">
         <div className="row frente">
           <div className="col-lg-6 col-md-8 col-12 mx-auto">
-          
-              <form className="custom-form login-form" role="form" method="post">
-                    <h2 className="hero-title text-center mb-4 pb-2">Trivia</h2>
-{/* aca tenememos q trabajrllo, xq tiene que aparecer la pregunta que se genera en CrearTrivia */}
-                    <h3 className="hero-title text-center mb-4 pb-2">¿Pregunta, aca esta la pregunta?</h3>
-                    <div className="col-lg-12 col-12">
-                        <div className="form-floating p-0">
-                            <input
-                                type="text"
-                                name="triviaRespuesta"
-                                id="triviaRespuesta"
-                                className="form-control"
-                                placeholder="TriviaRespuesta"
-                                required="true"
-                                />
+            <form className="custom-form login-form" role="form" method="post">
+              <h2 className="hero-title text-center mb-4 pb-2">Trivia</h2>
+              {/* aca tenememos q trabajrllo, xq tiene que aparecer la pregunta que se genera en CrearTrivia */}
+              <h3 className="hero-title text-center mb-4 pb-2">
+                ¿Pregunta, aca esta la pregunta?
+              </h3>
+              <div className="col-lg-6 col-md-8 col-12 mx-auto">
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    name="triviaRespuesta"
+                    id="triviaRespuesta"
+                    className="form-control"
+                    placeholder="TriviaRespuesta"
+                    required="true"
+                  />
 
-                            <label for="triviaRespuesta">Ingresa tu respuesta</label>
+                  <label htmlFor="triviaRespuesta">Ingresa tu respuesta</label>
 
-                            <button type="submit" className="form-control">Enviar Respuesta</button>
-                        </div>
-                    </div>
-                
-                </form>
+                  <button
+                    type="submit"
+                    className="btn custom-btn custom-border-btn w-100"
+                  >
+                    Enviar Respuesta
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
