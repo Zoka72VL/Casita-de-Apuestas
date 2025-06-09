6f9547121a4e4628efca39d1e85f6d6f30d10ebd
Zoka72VL <105391352+Zoka72VL@users.noreply.github.com>
2025-06-08 22:15:31 -0300

    Update AbmEyT.jsx
    correcion

--- casa-de-apuestas/frontend-react/src/components/profiles/admin/AbmEyT.jsx
+++ casa-de-apuestas/frontend-react/src/components/profiles/admin/AbmEyT.jsx
@@ -33,14 +33,7 @@ const AbmEyT = () => {
                   Modificar Evento
                 </NavLink>
               </div>
-              <div className="col-12 col-md-7 text-center mb-3">
-                <NavLink
-                  className="custom-btn btn mt-4 w-100"
-                  to="/crearplayer"
-                >
-                  Crear Jugador
-                </NavLink>
-              </div>
+
               
               
             </div>
